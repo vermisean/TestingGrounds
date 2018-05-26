bfc9b88226d499462505df58591761a04b7ecb07
Sean Duffy <sean.the.duffy@gmail.com>
2018-05-26 17:32:56 -0400

    Continued work on C++ AI

--- Source/TestingGrounds/Private/ChooseNextWaypoint.cpp
+++ Source/TestingGrounds/Private/ChooseNextWaypoint.cpp
@@ -1,11 +1,14 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "ChooseNextWaypoint.h"
-
+#include "BehaviorTree/BlackboardComponent.h"
 
 EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
-	UE_LOG(LogTemp, Warning, TEXT("AI in C++!"));
+	// Get blackboard data
+	UBlackboardComponent* BlackboardComp = OwnerComp.GetBlackboardComponent();
+	int Index = BlackboardComp->GetValueAsInt();
+	UE_LOG(LogTemp, Warning, TEXT("Waypoint Index: %i"), Index);
 
 	return EBTNodeResult::Succeeded;
 }
